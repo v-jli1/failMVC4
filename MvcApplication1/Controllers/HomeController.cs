4e5bdc1f1d74ece39dd550c1463b095bfce06b22
vm-v04 <dnpxjean1@hotmail.com>
2014-01-07 21:25:04 -0800

    push MvcApplication-F1 to remote:repo branch:master comment: push_Fail_MVCApplication

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -6,11 +6,11 @@ using System.Web.Mvc;
 
 namespace MvcApplication1.Controllers
 {
-    public class HomeController : Controller
+    //public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "jean well done.s1";
+            ViewBag.Message = "jean well done.0627";
 
             return View();
         }
